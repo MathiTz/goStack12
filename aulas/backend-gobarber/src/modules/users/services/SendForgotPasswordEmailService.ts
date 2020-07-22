8b16036fe3725185dd27575b8286494ccc3a6ce2
mathitz <matheusalves789@gmail.com>
2020-07-21 21:01:58 -0300

    Routes and controllers to update profile

--- aulas/backend-gobarber/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ aulas/backend-gobarber/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
-import { check } from 'prettier';
 import AppError from '@shared/errors/AppError';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
@@ -29,12 +29,29 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokensRepository.generate(user.id);
-
-    this.mailProvider.sendMail(
-      email,
-      'Pedido de recuperação de senha recebido',
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
     );
+
+    const { token } = await this.userTokensRepository.generate(user.id);
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
   }
 }
 
